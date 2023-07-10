817caa97920730559d40285013df5bedcc2930cc
RicardoHungJS <ricardohung92@gmail.com>
2023-07-10 16:41:26 -0500

    feat: adding apiKey of ipify from hosting

--- src/app/shared/services/ip-tracker/ip-tracker.service.ts
+++ src/app/shared/services/ip-tracker/ip-tracker.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { IPTrackingData, ipAddress } from '../../models/ipTrackerModels';
+import { environment } from 'src/enviroments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,7 @@ export class IpTrackerService {
 
   private apiUrl = 'https://geo.ipify.org/api/v2/country,city';
   private ipAddressUrl = 'https://api.ipify.org?format=json';
+  private apiKey = environment.apiKey;
 
   getIpAddress(): Observable<ipAddress> {
     const ipObject = this.http.get<ipAddress>(this.ipAddressUrl);
@@ -21,20 +23,15 @@ export class IpTrackerService {
     searchParameter: string,
     inputType: string
   ): Observable<IPTrackingData> {
-    /* Se entiende que para las apiKey y otros datos delicados se deben utilizar las variables de entorno del hosting y guardadas en un archivo 'enviroment' y 'env'
-    para ser consumidas en toda la aplicación sin embargo para efectos prácticos se dejará la apiKey de ipify guardada en una constante de este servicio. */
-
-    const apiKey = 'at_jJujWXqAOtcCLPdTLX6LGN8tKLykX';
-
     let url = '';
 
     switch (inputType) {
       case 'ipAdress':
-        url = `${this.apiUrl}?apiKey=${apiKey}&ipAddress=${searchParameter}`;
+        url = `${this.apiUrl}?apiKey=${this.apiKey}&ipAddress=${searchParameter}`;
         break;
 
       case 'url':
-        url = `${this.apiUrl}?apiKey=${apiKey}&domain=${searchParameter}`;
+        url = `${this.apiUrl}?apiKey=${this.apiKey}&domain=${searchParameter}`;
         break;
 
       default:
